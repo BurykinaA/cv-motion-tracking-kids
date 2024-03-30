fe5c9b0dd41c61430ff826b75a37eaf27655b821
Alina Burykina <alina4268@mail.ru>
2024-03-30 16:19:18 +0300

    new changes

--- backend/app/photo/routes.py
+++ backend/app/photo/routes.py
@@ -1,14 +1,31 @@
 from app.photo import photo
 from flask import request
-import requests
 from flask import current_app as app, make_response, jsonify, send_file
 from flask_cors import cross_origin
 
+import numpy as np
+from PIL import Image
+import base64
+import io
+
+from backend.app.utils.detection import detect_and_correct_errors
+
 
 @cross_origin()
-@photo.get("/api/photo")
+@photo.post("/api/photo")
 def make_correction():
-    # data_list = request
-    # print(data_list)
-    return make_response("dddd")
+    data_list = request.json  # Теперь ожидаем список JSON объектов
+    photos = []
 
+    for data in data_list:
+        image_bytes = base64.b64decode(data["photo"])
+        image_io = io.BytesIO(image_bytes)
+        image = Image.open(image_io)
+        numpy_array = np.array(image)
+
+        photos.append(numpy_array)
+
+    errors = detect_and_correct_errors(photos[0], photos[1])
+
+    response = {"frame": "".join(errors)}
+    return make_response(response)
---
+++ backend/app/utils/detection.py
@@ -0,0 +1,98 @@
+import mediapipe as mp
+
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+pose = mp_pose.Pose(static_image_mode=False)
+
+
+def cosine_distance(landmarks1, landmarks2):
+    if landmarks1 and landmarks2:
+        points1 = np.array([(lm.x, lm.y, lm.z) for lm in [landmarks1]])
+        points2 = np.array([(lm.x, lm.y, lm.z) for lm in [landmarks2]])
+        dot_product = np.dot(points1.flatten(), points2.flatten())
+        norm_product = np.linalg.norm(points1.flatten()) * np.linalg.norm(
+            points2.flatten()
+        )
+        similarity = dot_product / norm_product
+        return 1 - similarity
+    else:
+        return 1
+
+
+def detect_and_correct_errors(results_user, results_benchmark, threshold=0.5):
+    errors = []
+
+    # Legs
+    left_knee_user = results_user.pose_landmarks.landmark[
+        mp_pose.PoseLandmark.LEFT_KNEE
+    ]
+    left_knee_benchmark = results_benchmark.pose_landmarks.landmark[
+        mp_pose.PoseLandmark.LEFT_KNEE
+    ]
+    right_knee_user = results_user.pose_landmarks.landmark[
+        mp_pose.PoseLandmark.RIGHT_KNEE
+    ]
+    right_knee_benchmark = results_benchmark.pose_landmarks.landmark[
+        mp_pose.PoseLandmark.RIGHT_KNEE
+    ]
+
+    knee_distance_user = cosine_distance(left_knee_user, left_knee_benchmark)
+    knee_distance_benchmark = cosine_distance(right_knee_user, right_knee_benchmark)
+
+    if (
+        knee_distance_user * 100 < threshold
+        or knee_distance_benchmark * 100 < threshold
+    ):
+        errors.append("Неправильное расположение колен")
+
+    # Arms
+    left_elbow_user = results_user.pose_landmarks.landmark[
+        mp_pose.PoseLandmark.LEFT_ELBOW
+    ]
+    left_elbow_benchmark = results_benchmark.pose_landmarks.landmark[
+        mp_pose.PoseLandmark.LEFT_ELBOW
+    ]
+    right_elbow_user = results_user.pose_landmarks.landmark[
+        mp_pose.PoseLandmark.RIGHT_ELBOW
+    ]
+    right_elbow_benchmark = results_benchmark.pose_landmarks.landmark[
+        mp_pose.PoseLandmark.RIGHT_ELBOW
+    ]
+
+    elbow_distance_user = cosine_distance(left_elbow_user, left_elbow_benchmark)
+    elbow_distance_benchmark = cosine_distance(right_elbow_user, right_elbow_benchmark)
+
+    if (
+        elbow_distance_user * 100 < threshold
+        or elbow_distance_benchmark * 100 < threshold
+    ):
+        errors.append("Неправильное расположение рук")
+
+    # Torso
+    left_shoulder_user = results_user.pose_landmarks.landmark[
+        mp_pose.PoseLandmark.LEFT_SHOULDER
+    ]
+    left_shoulder_benchmark = results_benchmark.pose_landmarks.landmark[
+        mp_pose.PoseLandmark.LEFT_SHOULDER
+    ]
+    right_shoulder_user = results_user.pose_landmarks.landmark[
+        mp_pose.PoseLandmark.RIGHT_SHOULDER
+    ]
+    right_shoulder_benchmark = results_benchmark.pose_landmarks.landmark[
+        mp_pose.PoseLandmark.RIGHT_SHOULDER
+    ]
+
+    shoulder_distance_user = cosine_distance(
+        left_shoulder_user, left_shoulder_benchmark
+    )
+    shoulder_distance_benchmark = cosine_distance(
+        right_shoulder_user, right_shoulder_benchmark
+    )
+
+    if (
+        shoulder_distance_user * 100 < threshold
+        or shoulder_distance_benchmark * 100 < threshold
+    ):
+        errors.append("Неправильное расположение туловища")
+
+    return errors
